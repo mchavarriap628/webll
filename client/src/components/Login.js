05bf8f13a27401be745d4cb580fedaa4edd581e4
Manuel Chavarria <85698560+mchavarriap628@users.noreply.github.com>
2022-08-03 15:37:01 -0600

    Página de error 404

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,16 +1,18 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
 import useAuth from '../auth/useAuth'
 
 const userCredentials = {};
 
 export default function Login() {
 
+  const location = useLocation();
   const {login} = useAuth();
 
   return (
     <div>
         <h1>Login</h1>
-        <button onClick={() => login(userCredentials)}>Iniciar Sesión</button>
+        <button onClick={() => login(userCredentials, location.state?.from)}>Iniciar Sesión</button>
     </div>
   )
 }
--- client/src/routers/PrivateRoute.js
+++ client/src/routers/PrivateRoute.js
@@ -1,15 +1,16 @@
 import React from 'react'
-import { Redirect, Route } from 'react-router-dom';
+import { Redirect, Route, useLocation } from 'react-router-dom';
 import useAuth from '../auth/useAuth';
 import routes from './routes';
 
 
 export default function PrivateRoute({ hasRole: role, ...rest }) {
+  const location = useLocation();
   const { hasRole, isLogged } = useAuth();
 
   if (role && !hasRole(role)) return <Redirect to="/*" />
 
-  if (!isLogged()) return <Redirect to={routes.login} />
+  if (!isLogged()) return <Redirect to={{pathname: routes.login, state:{from: location}}} />
 
   return (
     <Route {...rest} />
